a920bd14666e926f0b109f873b91daa2701c1a3c
erincullen1122 <anniehall16@hotmail.com>
2026-03-04 05:18:11 +0000

    Update text

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-export const content = {
+export const npmcontent = {
   // ===== TEXT YOU'LL CHANGE MOST OFTEN =====
   workshopName: "Huggers Women's AA Meeting",
   siteTitle: "AA Huggers Women's Meeting",
